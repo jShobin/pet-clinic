f7b43cbd896e177a274c51efea433b7877229e0b
jShobin <oskarfabiann@gmail.com>
2020-03-07 10:07:12 +0100

    Added PetTypeMapService,PetTypeService. Changes in model (owner,speciality,vet) to create a relation between Owner and Pet.

---
+++ pet-clinic-data/src/main/java/oskar/spring/petclinic/services/map/PetTypeService.java
@@ -0,0 +1,7 @@
+package oskar.spring.petclinic.services.map;
+
+import oskar.spring.petclinic.model.PetType;
+import oskar.spring.petclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
